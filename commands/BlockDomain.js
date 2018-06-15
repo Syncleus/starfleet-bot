b5500ae18ed06841e9e45042f0c76c9006d94fd8
Tagadda <36127788+Tagadda@users.noreply.github.com>
2018-06-15 06:45:36 +0200

    Unfollow everybody from an instance when blockdomain

--- commands/BlockDomain.js
+++ commands/BlockDomain.js
@@ -11,6 +11,15 @@ class BlockDomain extends CommandBase {
     execute(msg, args) {
         this.client.block_domain(args[0]);
 
+        this.client.following_list().then((result) => {
+           for (const account of result) {
+               if (account.acct.split('@')[1] === args[0]) {
+                   this.client.unfollow(account.id);
+                   console.log("UNFOLLOW: " + account.acct);
+               }
+           }
+        });
+
         this.client.fav(msg.status.id);
     }
 }
